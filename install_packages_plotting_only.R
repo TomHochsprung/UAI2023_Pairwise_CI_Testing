7ee4a1f54a5bbc75f6bad1a9d9b98a23177c1895
Hochsprung, Tom <tom.hochsprung@dlr.de>
2023-02-15 20:36:24 +0000

    Upload New File

---
+++ install_packages_plotting_only.R
@@ -0,0 +1,6 @@
+install.packages("ggplot2", repos="http://cran.us.r-project.org")
+install.packages("dplyr", repos="http://cran.us.r-project.org")
+install.packages("tidyr", repos="http://cran.us.r-project.org")
+install.packages("ggpubr", repos="http://cran.us.r-project.org")
+install.packages("latex2exp", repos="http://cran.us.r-project.org")
+
