01661bd8c8df00d377555779837d8fb90257f271
Hochsprung, Tom <tom.hochsprung@dlr.de>
2023-02-16 10:01:04 +0000

    Replace model_sigma_2.R

--- model_sigma_2.R
+++ model_sigma_2.R
@@ -20,19 +20,22 @@ for (row_it in 1:dim_Y) {
 Sigma <- cbind(rbind(cbind(rbind(Sigma_X, matrix(numeric(dim_X * dim_Y), nrow = dim_Y, ncol = dim_X)),
                            rbind(matrix(numeric(dim_X * dim_Y), nrow = dim_X, ncol = dim_Y), Sigma_Y)), 
                      numeric(dim_X + dim_Y)), c(numeric(dim_X + dim_Y), 1))
-indices_non_zero <- sample(1:(dim_X * dim_Y), size = 10, replace = FALSE)
-indices_zero <- setdiff((1:(dim_X * dim_Y)), indices_non_zero)
+Sigma[1, dim_X + 1] <- rho
+Sigma[dim_X + 1, 1] <- rho
+indices_non_zero <- sample(2:(dim_X * dim_Y), size = 16, replace = FALSE)
+indices_zero <- setdiff((2:(dim_X * dim_Y)), indices_non_zero)
 non_zero_entries <- expand.grid(1:dim_X, 1:dim_Y)[indices_non_zero, ]
 indep_set <- expand.grid(1:dim_X, 1:dim_Y)[indices_zero, ]
-for (nze_iterate in 1:10) {
+for (nze_iterate in 1:16) {
   Sigma[non_zero_entries[nze_iterate, 1],
         dim_X + non_zero_entries[nze_iterate, 2]] <- 
-    runif(1, rho/100, rho/10) * sign(runif(1, -1, 1))
+    rho / 16
   Sigma[dim_X + non_zero_entries[nze_iterate, 2],
         non_zero_entries[nze_iterate, 1]] <-
     Sigma[non_zero_entries[nze_iterate, 1],
           dim_X + non_zero_entries[nze_iterate, 2]]
 }
+min(eigen(Sigma)$values)
 # generate multivariate normal data according to covariance matrix Sigma and
 # mean 0
 data <- rmvnorm(n, sigma = Sigma)
