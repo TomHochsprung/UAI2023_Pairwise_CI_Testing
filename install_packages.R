fbf50513e43f1554398aad6f2426225f010baec4
Hochsprung, Tom <tom.hochsprung@dlr.de>
2023-06-08 08:30:39 +0000

    Delete install_packages.R

--- install_packages.R
+++
@@ -1,4 +0,0 @@
-install.packages("GeneralisedCovarianceMeasure", repos="http://cran.us.r-project.org")
-install.packages("mvtnorm", repos="http://cran.us.r-project.org")
-install.packages("energy", repos="http://cran.us.r-project.org")
-
