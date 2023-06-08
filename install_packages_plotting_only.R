508f1a1222927e346d3f3d8d951500f64e4b97f9
Hochsprung, Tom <tom.hochsprung@dlr.de>
2023-06-08 08:33:55 +0000

    Delete install_packages_plotting_only.R

--- install_packages_plotting_only.R
+++
@@ -1,6 +0,0 @@
-install.packages("ggplot2", repos="http://cran.us.r-project.org")
-install.packages("dplyr", repos="http://cran.us.r-project.org")
-install.packages("tidyr", repos="http://cran.us.r-project.org")
-install.packages("ggpubr", repos="http://cran.us.r-project.org")
-install.packages("latex2exp", repos="http://cran.us.r-project.org")
-
